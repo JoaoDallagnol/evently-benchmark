14c17cc34a3789871949c5b4688224183cc3bf5f
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-01-08 20:40:41 -0300

    Initializing java repo

---
+++ api-java/src/main/java/com/api/evently_java/EventlyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.api.evently_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventlyJavaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EventlyJavaApplication.class, args);
+	}
+
+}
---
+++ api-java/src/test/java/com/api/evently_java/EventlyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.evently_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlyJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
